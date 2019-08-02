11cfec7d1fba39461a963ac8aaa625476c194bfe
Stas Khavruk <23425714+Jeidoz@users.noreply.github.com>
2019-08-02 15:21:15 +0300

    Add project files.

---
+++ DiaryInstaBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiaryInstaBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
