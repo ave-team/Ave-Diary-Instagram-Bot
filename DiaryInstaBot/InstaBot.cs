25c47af3f17cee7e9eeb4cbcdd872985c4076386
Stas Khavruk <23425714+Jeidoz@users.noreply.github.com>
2019-08-02 15:30:40 +0300

    Added Instagram Bot class prototype with main field and constructor.

---
+++ DiaryInstaBot/InstaBot.cs
@@ -0,0 +1,38 @@
+﻿using InstagramApiSharp.API;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Logger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AveDiaryInstaBot
+{
+    public class InstaBot
+    {
+        private const string AveDiaryApiBaseLink = "https://avediary.online/api.php";
+        private IInstaApi instaApi;
+        private IRequestDelay instaApiDelay;
+        private UserSessionData loginData;
+
+        public InstaBot(string username, string password)
+        {
+            this.loginData = new UserSessionData
+            {
+                UserName = username,
+                Password = password
+            };
+
+            InitializeInstaApi();
+        }
+        private void InitializeInstaApi()
+        {
+            this.instaApiDelay = RequestDelay.FromSeconds(2, 2);
+            this.instaApi = InstaApiBuilder.CreateBuilder()
+                 .SetUser(this.loginData)
+                 .UseLogger(new DebugLogger(LogLevel.Exceptions))
+                 .SetRequestDelay(this.instaApiDelay)
+                 .Build();
+        }
+    }
+}
