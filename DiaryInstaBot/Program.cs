2598b984492a00ae90c26904edeca30ce38209ee
Stas Khavruk <23425714+Jeidoz@users.noreply.github.com>
2019-08-02 15:31:03 +0300

    Added arguments validation and bot initialization

--- DiaryInstaBot/Program.cs
+++ DiaryInstaBot/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AveDiaryInstaBot;
+using System;
+using System.Text;
 
 namespace DiaryInstaBot
 {
@@ -6,7 +8,23 @@ namespace DiaryInstaBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            if (!IsValidParams(ref args))
+                return;
+
+            InstaBot bot = new InstaBot(args[0], args[1]);
+        }
+
+        static bool IsValidParams(ref string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Wrong startup argumetns. Use by next template:");
+                Console.WriteLine("dotnet .\\AveDiaryInstaBot.dll Username Password");
+                return false;
+            }
+            return true;
         }
     }
 }
